c56acbceaa71306339d625c98503e6aa86186fc7
Gerald-97 <geraldinyiama@gmail.com>
2020-01-21 17:00:02 +0100

    installed nodemailer dependencies

--- routes/testRoute.js
+++ routes/testRoute.js
@@ -4,8 +4,10 @@ const router = express.Router();
 // const appCreate = require("../controllers/admin/appCreate")
 const assCreate = require("../controllers/admin/assessment")
 const auth = require("../middleware/token");
+const calcAns = require("../controllers/admin/scoring")
 
 router.get("/all-test", auth, assCreate.AssessmentDisplay);
+router.post("/calc-test", auth, calcAns)
 router.post("/test", auth, assCreate.AssessmentEntry);
 router.get("/one-test/:id", assCreate.AssessmentDisplayOne);
 router.put("/test/:id", assCreate.AssessmentUpdate);
