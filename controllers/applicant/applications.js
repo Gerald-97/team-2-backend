c4eeadbfdf173cac828d1a8f7631c5048731507c
Akinpelumi <54557636+Akinpelumi@users.noreply.github.com>
2020-01-21 17:00:39 +0100

    Update applications.js

--- controllers/applicant/applications.js
+++ controllers/applicant/applications.js
@@ -1,31 +1,53 @@
 const Application = require("../../models/application");
-const Applicant = require("../../models/appCreate");
 const User = require("../../models/user");
 var nodemailer = require("nodemailer");
+var smtpTransport = require('nodemailer-smtp-transport');
 const morgan = require("morgan");
 var cloudinary = require("cloudinary").v2;
-
-var transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: `${process.env.EMAIL}`,
-    password: `${process.env.PASSWORD}`
-  }
-});
 
 // New Application Entry
 const newApp = async (req, res, next) => {
   try {
-    var deadlineApp = await Application.find({
-      isActive: true
-    });
-    if (deadlineApp.date < Date.now()) {
-      res.status(401).json({
-        message: "Applications are closed for this cycle"
-      })
-    } else {
+    const {
+      firstName,
+      lastName,
+      email,
+      birthday,
+      address,
+      school,
+      courseOfStudy,
+      cgpa
+    } = req.body;
+    const upload = req.files.upload;
 
-      const {
+    // const filetypes = /pdf|doc|docx/;
+    // const extname = filetypes.test(path.extname(upload.originalname).toLowerCase());
+    // const mimetype = filetypes.test(upload.mimetype);
+    // if (extname && mimetype) {
+    //   return cb(null, true);
+    // } else {
+    //   cb('Error: Put in the required format')
+    // }
+
+    upload.mv("public/cv/" + upload.name, function (err) {
+      if (err) {
+        console.log("Couldn't upload");
+        console.log(err);
+      } else {
+        console.log("Saved!");
+      }
+    });
+
+    const data = await User.findOne({
+      email
+    });
+
+    if (data) {
+      return res.status(409).json({
+        message: `Application for ${email} has been received already`
+      });
+    } else {
+      const newEntry = await new Application({
         firstName,
         lastName,
         email,
@@ -33,75 +55,42 @@ const newApp = async (req, res, next) => {
         address,
         school,
         courseOfStudy,
-        cgpa
-      } = req.body;
-      const file = req.files.file;
-
-      // const filetypes = /pdf|doc|docx/;
-      // const extname = filetypes.test(upload.extname(upload.originalname).toLowerCase());
-      // const mimetype = filetypes.test(upload.mimetype);
-      // if (extname && mimetype) {
-      //   return cb(null, true);
-      // } else {
-      //   cb('Error: Put in the required format')
-      // }
-
-      await file.mv("public/cv/" + file.name);
-      const data = await Application.findOne({
-        email
+        cgpa,
+        upload
       });
+      await newEntry.save();
 
-      if (data) {
-        return res.status(409).json({
-          message: `Application for ${email} has been received already`
-        });
-      } else {
 
-        // const batchID = await Applicant.findOne({
-        //   isActive: true
-        // })
-        // var batchNumber = batchID.batch
+      var transporter = nodemailer.createTransport(smtpTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        auth: {
+          user: 'akintundeakinpelumi@gmail.com',
+          pass: '2414adec'
+        }
+      }));
 
-        const newEntry = await new Application({
-          firstName,
-          lastName,
-          email,
-          birthday,
-          address,
-          school,
-          courseOfStudy,
-          cgpa,
-          file
-        });
+      var mailOptions = {
+        from: 'akintundeakinpelumi@gmail.com',
+        to: email,
+        subject: 'Your application: Software Developer Academy',
+        text: `Thank you for your interest in a career opportunity at Enyata. We have received and we are currently reviewing your application. Thank you for taking the time to fill in the application form. We encourage you to visit our website at www.enyata.com for more information.
+              
+              
+        Enyata Recruitment Team`
+      };
 
-        await newEntry.save();
-        const content = `
-        <p>Dear ${firstName},</p>
-        <p>Thank you for your interest in a career opportunity at Enyata.</p>
-        <p> We have received and we are currently reviewing your application.</p>
-        <p> Thank you for taking the time to fill in the application form. We encourage you to visit our website at <a href='enyata.com'>enyata.com</a> for more information.</p>
-        <br>
-        <p>Enyata Recruitment Team</p>`;
-
-        var message = {
-          from: '"Enyata Software Engineering" <anitaogechi9@gmail.com>',
-          to: `${email}`,
-          subject: "Your application: Software Developer Academy",
-          html: content
-        };
-
-        await transporter.sendMail(message, function (error, info) {
-          if (error) {
-            return next(error);
-          } else {
-            alert("Email sent: " + info.response);
-          }
-        });
-        return res.status(201).json({
-          message: "Thank you for submitting your application, we will get back to you",
-          newEntry
-        });
-      }
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          return  res.status(400).json({
+            message:  'Application not sent'
+          })
+        } else {
+          return  res.status(200).json({
+            message:  'Application sent! Check your mail to confirm.'
+          })
+        }
+      });
     }
   } catch (err) {
     return next(err);
